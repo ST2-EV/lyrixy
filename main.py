82bca4ba7a72fb192f7966d9a0daa934246dab1d
ST2-EV <steve.s.paul22@gmail.com>
2020-07-05 00:09:39 +0530

    small bug-fix

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import force_align
 import extract_proper_frames
 import create_video
 
-def run_algo(path_to_audio_file, path_to_lyrics):
+def run_algo(path_to_audio_file, path_to_lyrics, dest_path="lyrixy-out.mp4"):
 	aligned_data = force_align.align_process(path_to_audio_file, path_to_lyrics)
 	extract_proper_frames.decide_frames(aligned_data)
-	create_video.create_video(video_data, path_to_audio_file)
+	create_video.create_video(video_data, path_to_audio_file, dest_path)
   	print('It took', time.time()-start, 'seconds.')
+
+if __name__ == "__main__":
+	run_algo()
